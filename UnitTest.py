5672a85d3c2b0a637de09dba0495ac2eef0e645c
ryonlunar <adhimas.bimo@gmail.com>
2025-11-13 18:40:01 +0700

    fix : identation unit test

--- UnitTest.py
+++ UnitTest.py
@@ -5,4 +5,4 @@ from tests.LogObjectTest import run_log_object_tests
 if __name__ == '__main__':
 	run_lock_manager_tests()
 	run_log_object_tests()
-  run_begin_transaction_tests()
+	run_begin_transaction_tests()
