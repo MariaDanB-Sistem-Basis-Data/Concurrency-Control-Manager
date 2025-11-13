d30208aef7df09e9bcc6e89cea44e433fb24d715
Julius <juliusarthur2005@gmail.com>
2025-11-13 13:31:16 +0700

    feat: deadlock detecting graph and algorithm

--- model/DeadlockDetector.py
+++ model/DeadlockDetector.py
@@ -1,12 +1,56 @@
 class DeadlockDetector:
     def __init__(self):
-        print("Deadlock Detector")
+        # Wait graph. Ti -> set of Tx (Ti menunggu set of Tx melepas lock)
+        self.graph = {}
 
-    def validate_object(object, transaction_id, action):
-        # memeriksa apakah action pada object dengan transaction_id bisa dilakukan
-        print(f"Validate action {action} on object {object} for transaction_id {transaction_id}")
+        print("Deadlock Detector")
     
+    def add_wait_edge(self, from_trx, to_trx):
+        if from_trx == to_trx:
+            return 
+        
+        self.graph.setdefault(from_trx, set()).add(to_trx)
+        self.graph.setdefault(to_trx, set())
+
+    def remove_add_edge(self, from_tx, to_tx):
+        if from_tx in self.graph:
+            self.graph[from_tx].discard(to_tx)
+
+    def _find_cycles(self):
+        visited = set()
+        stack = []
+        onstack = set()
+        cycles = []
+
+        def dfs(u):
+            visited.add(u)
+            stack.append(u)
+            onstack.add(u)
+            for v in self.graph.get(u, ()):
+                if v not in visited:
+                    dfs(v)
+                elif v in onstack:
+                    try:
+                        idx = stack.index(v)
+                        cycle = stack[idx:].copy()
+                        cycles.append(cycle)
+                    except ValueError:
+                        pass
+            stack.pop()
+            onstack.remove(u)
+
+        for node in list(self.graph.keys()):
+            if node not in visited:
+                dfs(node)
+        return cycles
+
+
     def check_deadlock(self):
-        # memeriksa apakah ada deadlock
-        print("Checking for deadlock")
-        return True
+        # memeriksa apakah ada deadlock 
+        cycle = self._find_cycles()
+        has_deadlock = len(cycle) > 0
+
+        if (has_deadlock):
+            print(f"Deadlock Detected: {cycle}")
+
+        return has_deadlock, cycle
